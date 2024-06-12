d982dea9f035abc2dc60beb8c27fb8abfe370666
Bene1687 <benedicte.challet@gmail.com>
2024-06-12 15:46:43 +0100

    Wrote down all the requisite for this project.

--- The Slot Machine/Program.cs
+++ The Slot Machine/Program.cs
@@ -6,5 +6,12 @@
         {
             Console.WriteLine("Hello, World!");
         }
-    }
+    }// Design a game where the user can play a make believe slot machine//
+    // How do I want my slot machine to work?//
+    // Fill array with random numbers//
+    // Output that array//
+    // Check if middle row is all the same//
+    // Output win/lose//
+    // Keep track of money//
+    // Implement more game modes (multiple lines/diagonals...)//
 }
