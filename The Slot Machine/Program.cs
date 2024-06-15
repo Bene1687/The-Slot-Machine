a60ac3c8f7a0588a32e26957b40ae50e0e1de382
Bene1687 <benedicte.challet@gmail.com>
2024-06-15 21:51:47 +0100

    Creation of a menu for the Slot Game, first try.

--- The Slot Machine/Program.cs
+++ The Slot Machine/Program.cs
@@ -3,15 +3,19 @@
     internal class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
+        {//Create a menu for the game
+            Console.WriteLine("SLOT MACHINE");
+            Console.WriteLine();
+            Console.WriteLine("Player decide how many lines to play, £1 per line");
+            Console.ReadLine();
         }
-    }// Design a game where the user can play a make believe slot machine//
-    // How do I want my slot machine to work?//
-    // Fill array with random numbers//
-    // Output that array//
-    // Check if middle row is all the same//
-    // Output win/lose//
-    // Keep track of money//
-    // Implement more game modes (multiple lines/diagonals...)//
+    }//Design a game where the user can play a make believe slot machine
+    //How do I want my slot machine to work?
+    //What are the winning combinations?
+    //Fill array with random numbers
+    //Output that array
+    //Check if middle row is all the same
+    //Output win/lose
+    //Keep track of money
+    //Implement more game modes (multiple lines/diagonals...)
 }
