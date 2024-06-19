6fe6ceabc5e2802bcb42cdcfd1e5497d77966abb
Bene1687 <benedicte.challet@gmail.com>
2024-06-19 22:37:58 +0100

    More lines options created, re organised the look of it in the console to make it more readable.

--- The Slot Machine/Program.cs
+++ The Slot Machine/Program.cs
@@ -10,7 +10,23 @@
             Console.WriteLine();
             Console.WriteLine("LINE OPTIONS : ");
             Console.WriteLine();
-            Console.WriteLine("Top line, middle line, bottom line, all lines");
+            Console.WriteLine(" 1 = Top horizontal line");
+            Console.WriteLine();
+            Console.WriteLine(" 2 = Middle horizontal line");
+            Console.WriteLine();
+            Console.WriteLine(" 3 = Bottom horizontal line");
+            Console.WriteLine();
+            Console.WriteLine(" 4 = Top vertical line");
+            Console.WriteLine();
+            Console.WriteLine(" 5 = Middle vertical line");
+            Console.WriteLine();
+            Console.WriteLine(" 6 = Bottom vertical line");
+            Console.WriteLine();
+            Console.WriteLine(" 7 = First diagonal line");
+            Console.WriteLine();
+            Console.WriteLine(" 8 = Second diagonal line");
+            Console.WriteLine();    
+            Console.WriteLine(" 9 = all lines");
             Console.ReadLine();
         }
     }//Design a game where the user can play a make believe slot machine
