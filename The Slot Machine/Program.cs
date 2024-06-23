40debc45fdd033fde13e35aabfa47d7b1e2a7117
Bene1687 <benedicte.challet@gmail.com>
2024-06-23 21:39:13 +0100

    Modification to the menu presentation.

--- The Slot Machine/Program.cs
+++ The Slot Machine/Program.cs
@@ -8,25 +8,8 @@
             Console.WriteLine();
             Console.WriteLine("Player decide which lines to play, £1 per line");
             Console.WriteLine();
-            Console.WriteLine("LINE OPTIONS : ");
+            Console.WriteLine("LINE OPTIONS : 1 = Top horizontal, 2 = Middle horizontal, 3 = Bottom horizontal, 4 = Top vertical, 5 = Middle vertical, 6 = Bottom vertical, 7 = First diagonal, 8 = Second diagonal, 9 = all lines");
             Console.WriteLine();
-            Console.WriteLine(" 1 = Top horizontal line");
-            Console.WriteLine();
-            Console.WriteLine(" 2 = Middle horizontal line");
-            Console.WriteLine();
-            Console.WriteLine(" 3 = Bottom horizontal line");
-            Console.WriteLine();
-            Console.WriteLine(" 4 = Top vertical line");
-            Console.WriteLine();
-            Console.WriteLine(" 5 = Middle vertical line");
-            Console.WriteLine();
-            Console.WriteLine(" 6 = Bottom vertical line");
-            Console.WriteLine();
-            Console.WriteLine(" 7 = First diagonal line");
-            Console.WriteLine();
-            Console.WriteLine(" 8 = Second diagonal line");
-            Console.WriteLine();    
-            Console.WriteLine(" 9 = all lines");
             Console.ReadLine();
         }
     }//Design a game where the user can play a make believe slot machine
