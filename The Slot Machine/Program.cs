7d4e407cbbe7ca2ed2dde50b05c31e0b146d5751
Bene1687 <benedicte.challet@gmail.com>
2024-06-23 21:47:28 +0100

    Constantes of the menu, first steps, created.

--- The Slot Machine/Program.cs
+++ The Slot Machine/Program.cs
@@ -3,14 +3,24 @@
     internal class Program
     {
         static void Main(string[] args)
-        {//Create a menu for the game
+        {
+            const int TOP_HORIZONTAL = 1;
+            const int MIDDLE_HORIZONTAL = 2;
+            const int BOTTOM_HORIZONTAL = 3;    
+            const int TOP_VERTICAL = 4; 
+            const int MIDDLE_VERTICAL = 5;
+            const int BOTTOM_VERTICAL = 6;
+            const int FIRST_DIAGONAL = 7;
+            const int SECOND_DIAGONAL = 8;
+            const int ALL_LINES = 9;
+            //Create a menu for the game
             Console.WriteLine("SLOT MACHINE");
             Console.WriteLine();
             Console.WriteLine("Player decide which lines to play, £1 per line");
             Console.WriteLine();
             Console.WriteLine("LINE OPTIONS : 1 = Top horizontal, 2 = Middle horizontal, 3 = Bottom horizontal, 4 = Top vertical, 5 = Middle vertical, 6 = Bottom vertical, 7 = First diagonal, 8 = Second diagonal, 9 = all lines");
             Console.WriteLine();
-            Console.ReadLine();
+            int answer = Convert.ToInt32(Console.ReadLine());
         }
     }//Design a game where the user can play a make believe slot machine
     //How do I want my slot machine to work?
