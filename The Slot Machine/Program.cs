f6ceb2eccf146eac17a6d18fd2dc7135f3fd1b65
Bene1687 <benedicte.challet@gmail.com>
2024-09-26 14:33:42 +0100

    Modifications done for the notations to have it clearer and more on point. Also, random numbers to fill the Grid estend to print the number 3 properly.

--- The Slot Machine/Program.cs
+++ The Slot Machine/Program.cs
@@ -3,7 +3,16 @@
     internal class Program
     {
         static void Main(string[] args)
-        {
+        {//Design a game where the user can play a make believe slot machine
+         //How do I want my slot machine to work?
+         //What are the winning combinations?
+         //Fill array with random numbers
+         //Output that array
+         //Check if middle row is all the same
+         //Output win/lose
+         //Keep track of money
+         //Implement more game modes (multiple lines/diagonals...)
+         //Create a menu for the game
             const int ALL_HORIZONTALS = 1;
             const int ALL_VERTICALS = 2;
             const int CENTER_LINE = 3;    
@@ -11,24 +20,28 @@
             const int COLUMNS_HORIZONTALS = 3;
             const int COLUMNS_VERTICALS = 3;
             Random number = new Random();
-            //Create a menu for the game
+            
             Console.WriteLine("SLOT MACHINE");
             Console.WriteLine();
             Console.WriteLine("Player decide which lines to play, £1 per line");
             Console.WriteLine();
             Console.WriteLine("LINE OPTIONS : 1 = All horizontals, 2 = All verticals, 3 = Center line, 4 = diagonals");
             Console.WriteLine();
+
             int answer = Convert.ToInt32(Console.ReadLine());
+
             Console.WriteLine("you entered : " + answer);
             int[,] Grid = new int[3,3];
+
+            //Fill the Grid with random number between 1 and 4
             for (int lineIndex = 0; lineIndex < COLUMNS_HORIZONTALS; lineIndex++)
             {
                 for (int columnIndex = 0; columnIndex  < COLUMNS_VERTICALS; columnIndex++)
                 {
-                    //first couple of loops
-                    Grid[lineIndex, columnIndex] = number.Next(1, 3);
+                    Grid[lineIndex, columnIndex] = number.Next(1,4);
                 }
-            }  //another couple of lopops
+            }  
+            //Print the content of the Grid
             for (int lineIndex = 0; lineIndex < COLUMNS_HORIZONTALS; lineIndex++)
             {
                 for (int columnIndex = 0; columnIndex  < COLUMNS_VERTICALS; columnIndex++)
@@ -36,7 +49,7 @@
                    Console.WriteLine(Grid[lineIndex, columnIndex]);
                 }
             }
-                    //another couple of loop
+            //Check for wins
             for (int lineIndex = 0; lineIndex < COLUMNS_HORIZONTALS; lineIndex++)
             {
                 for (int columnIndex = 0; columnIndex  < COLUMNS_VERTICALS; columnIndex++)
@@ -56,13 +69,5 @@
 
 
         }
-    }//Design a game where the user can play a make believe slot machine
-    //How do I want my slot machine to work?
-    //What are the winning combinations?
-    //Fill array with random numbers
-    //Output that array
-    //Check if middle row is all the same
-    //Output win/lose
-    //Keep track of money
-    //Implement more game modes (multiple lines/diagonals...)
+    }
 }
