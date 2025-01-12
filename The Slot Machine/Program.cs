40e5c0d6c3ae082702e88bd1fce964d09babb6c6
Bene1687 <benedicte.challet@gmail.com>
2025-01-12 21:40:48 +0000

    Removed the symbol ! in front of win in the block CENTRE_LINE to check for win properly.

--- The Slot Machine/Program.cs
+++ The Slot Machine/Program.cs
@@ -115,7 +115,7 @@
                         }
                     }
                 }
-                if (!win)
+                if (win)
                 {
                     Console.WriteLine();
                     Console.Write("YOU WIN");
